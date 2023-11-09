ea9cd09722f8b7823b9e579a1c33f0dcdddbb0a3
mxrqxxz <4186790@alu.murciaeduca.es>
2023-11-09 19:35:36 +0100

    Saltos terminados

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.onload = function () {
     let imagenSamurai;
     let x = 450; // Coordenada ninja inicial
     let y = 470; // Coordenada ninja inicial
+    let ySuelo = 540;
 
     function limpiarLienzo() {
         ctx.clearRect(0, 0, 1000, 600);
@@ -19,7 +20,7 @@ window.onload = function () {
     function samurai(x_, y_) {
         this.x = x_;
         this.y = y_;
-        this.velocidad = 2;
+        this.velocidad = 3;
 
         // -- Movimientos --
 
@@ -33,15 +34,16 @@ window.onload = function () {
         this.estaticoDerecha = false;
         this.estaticoIzquierda = false;
         this.saltoDerecha = false;
+        this.saltoIzquierda = false;
 
         // -- Animaciones --
 
         this.posicionAnimacion = 0;
         this.totalAnimacion = 1;
-        this.animacionSamurai = /* Estatico Derecha */[[1150, 11], [1278, 11], [1406, 11], [1534, 11], [1662, 11], [1789, 11], /* Estatico Izquierda */[1053, 11], [925, 11], [797, 11], [669, 11], [541, 11], [414, 11], /* Movimiento Derecha */[2054, 274], [1926, 275], [1798, 274], [1670, 273], [1543, 275], [1414, 275], [1286, 274], [1157, 274], /* Movimiento Izquierda */[1048, 273], [919, 274], [791, 275], [662, 275], [535, 273], [407, 274], [279, 275], [150, 274], /* Salto Derecha */ [1146, 911], [1282, 917], [1410, 920], [1537, 900], [1667, 905], [1795, 906], [1920, 907], [2052, 933], [2180, 933]];
+        this.animacionSamurai = /* Estatico Derecha */[[1150, 11], [1278, 11], [1406, 11], [1534, 11], [1662, 11], [1789, 11], /* Estatico Izquierda */[1053, 11], [925, 11], [797, 11], [669, 11], [541, 11], [414, 11], /* Movimiento Derecha */[2054, 274], [1926, 275], [1798, 274], [1670, 273], [1543, 275], [1414, 275], [1286, 274], [1157, 274], /* Movimiento Izquierda */[1048, 273], [919, 274], [791, 275], [662, 275], [535, 273], [407, 274], [279, 275], [150, 274], /* Salto Derecha */[1146, 911], [1282, 917], [1410, 920], [1537, 900], [1667, 905], [1795, 906], [1920, 907], /* Salto Izquierda */[1057, 911], [930, 917], [803, 920], [680, 900], [546, 905], [418, 906], [295, 907]];
 
         this.posicionTamaño = 0;
-        this.tamañoAnimacion = /* Estatico */[[58, 70], /* Corriendo */[54, 64], /* Saltando */[56,67], [47,61], [47, 58], [42,72], [46,59], [46,59], [44,65], [42,45], [39,45]];
+        this.tamañoAnimacion = /* Estatico */[[58, 70], /* Corriendo */[54, 64], /* Saltando */[56, 67], [47, 61], [47, 58], [42, 72], [46, 59], [46, 59], [44, 65]];
 
         this.tamañoX = this.tamañoAnimacion[this.posicionTamaño][0];
         this.tamañoY = this.tamañoAnimacion[this.posicionTamaño][1];
@@ -109,15 +111,55 @@ window.onload = function () {
         ninja.estaticoDerecha = true;
     }
 
+    function moverArriba() {
+
+        // Solo será para el salto
+        ninja.y -= 50;
+        ninja.velocidad = 5;
+    }
+
+    function moverAbajo() {
+
+        // Para caer del salto
+        ninja.y += 50;
+    }
+
     function permitirSalto() {
-        if (ninja.derecha || ninja.estaticoDerecha){
+
+        // Dependiendo de a donde mire el ninja saltamos a un lado o a otro
+        if (ninja.derecha || ninja.estaticoDerecha) {
             ninja.saltoDerecha = true;
+        } else if (ninja.izquierda || ninja.estaticoIzquierda) {
+            ninja.saltoIzquierda = true;
+        }
+    }
+
+    function terminarSalto() {
+        if (ninja.saltoDerecha){
+            ninja.saltoDerecha = false;            // Paramos salto
+            ninja.velocidad = 3;                   // Reajustamos la velocidad que habíamos subido con moverArriba();
+            if (ninja.derecha) {
+                ninja.derechaAnimacion = true;     // Si el ninja corre, activamos animacion manualmente, ya que si no se ha dejado de pulsar la tecla, el valor sigue siendo false al haber desactivado la animación de correr para ejecutar la del salto en saltarDerecha()
+            }
+            ninja.posicionTamaño = 0;              // Reiniciamos tamaño, ya que si saltamos de nuevo podría hacer conflicto, mejor que cada animación use su tamaño al inicio de la misma
+        } else {                                   // Lo mismo pero a la izquierda 
+            ninja.saltoIzquierda = false;
+            ninja.velocidad = 3;
+            if (ninja.izquierda) {
+                ninja.izquierdaAnimacion = true;
+            }
+            ninja.posicionTamaño = 0;
         }
     }
 
     function saltarDerecha() {
-        ninja.derechaAnimacion = false;
+        ninja.derechaAnimacion = false;     // Paramos las animaciones para que no hagan conflicto con la del salto
         ninja.estaticoDerecha = false;
+    }
+
+    function saltarIzquierda() {
+        ninja.izquierdaAnimacion = false;
+        ninja.estaticoIzquierda = false;
     }
 
     function activaMovimiento(evt) {
@@ -166,8 +208,12 @@ window.onload = function () {
             moverIzquierda();
         }
 
-        if (ninja.saltoDerecha){
+        if (ninja.saltoDerecha) {
             saltarDerecha();
+        }
+
+        if (ninja.saltoIzquierda) {
+            saltarIzquierda();
         }
 
         ctx.drawImage(
@@ -215,50 +261,87 @@ window.onload = function () {
     }
 
     function animacionSaltoDerecha() {
-        
-        if (ninja.posicionTamaño < 2 || ninja.posicionTamaño > 11){
+
+        /*
+
+        No he podido hacerlo de la misma manera que las demás animaciones, y la verdad
+        es que desconozco el motivo, pero tras horas de intentos he decidido hacerlo por "la cuenta de la vieja"
+
+        */
+
+        // Es una manera para dar los valores la primera vez que se ejecuta la animación
+        if (ninja.posicionTamaño < 2 || ninja.posicionTamaño > 8) { 
             ninja.posicionTamaño = 2;
             ninja.posicionAnimacion = 28;
         }
 
-        ninja.posicionTamaño++;
-        ninja.posicionAnimacion++;
-
-        if (ninja.posicionTamaño > 11){
-            ninja.saltoDerecha = false;
+        // La mitad de la animación que salte y que la otra mitad caiga del salto
+        if (ninja.posicionAnimacion > 28 && ninja.posicionAnimacion < 32) {
+            moverArriba();
+        } else if (ninja.posicionAnimacion > 31) {
+            moverAbajo();
         }
+
+        // Para controlar la finalización de la animación
+        if (ninja.posicionAnimacion < 34) {
+            ninja.posicionTamaño++;
+            ninja.posicionAnimacion++;
+        } else {
+            terminarSalto();
+        }
+
+
     }
-    
+
+    function animacionSaltoIzquierda() {        // Comentada arriba
+        if (ninja.posicionTamaño < 2 || ninja.posicionTamaño > 8) {
+            ninja.posicionTamaño = 2;
+            ninja.posicionAnimacion = 35;
+        }
+
+        if (ninja.posicionAnimacion > 35 && ninja.posicionAnimacion < 39) {
+            moverArriba();
+        } else if (ninja.posicionAnimacion > 38) {
+            moverAbajo();
+        }
+
+        if (ninja.posicionAnimacion < 41) {
+            ninja.posicionTamaño++;
+            ninja.posicionAnimacion++;
+        } else {
+            terminarSalto();
+        }
+
+
+    }
+
     function animacion() {
 
-        if (!ninja.estaticoIzquierda && !ninja.izquierdaAnimacion && !ninja.estaticoDerecha && !ninja.derechaAnimacion && !ninja.saltoDerecha) {
-            ninja.estaticoDerecha = true;
+        if (!ninja.estaticoIzquierda && !ninja.izquierdaAnimacion && !ninja.estaticoDerecha && !ninja.derechaAnimacion && !ninja.saltoDerecha && !ninja.saltoIzquierda) {
+            ninja.estaticoDerecha = true;   // Por defecto que mire a la derecha
         }
 
         if (ninja.derecha && ninja.izquierda) {
+
+            // Si el jugador se mueve a ambas direcciones a la vez el personaje se quedará quieto con la animación estática a la derecha
             ninja.derechaAnimacion = false;
             ninja.izquierdaAnimacion = false;
             ninja.estaticoDerecha = true;
         }
 
-        if (ninja.estaticoIzquierda) {
-            animacionMirandoIzquierda();
-        }
-
-        if (ninja.izquierdaAnimacion) {
-            animacionCorrerIzquierda();
-        }
-
-        if (ninja.estaticoDerecha) {
-            animacionMirandoDerecha();
-        }
-
-        if (ninja.derechaAnimacion) {
-            animacionCorrerDerecha();
-        }
-
-        if (ninja.saltoDerecha){
+        // Ejecutamos la animación que corresponda
+        if (ninja.saltoDerecha) {
             animacionSaltoDerecha();
+        } else if (ninja.saltoIzquierda) {
+            animacionSaltoIzquierda();
+        } else if (ninja.estaticoIzquierda) {
+            animacionMirandoIzquierda();
+        } else if (ninja.izquierdaAnimacion) {
+            animacionCorrerIzquierda();
+        } else if (ninja.estaticoDerecha) {
+            animacionMirandoDerecha();
+        } else if (ninja.derechaAnimacion) {
+            animacionCorrerDerecha();
         }
 
     }
